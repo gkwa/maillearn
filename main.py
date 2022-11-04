27fd5285bb96bd5d1c0c9be0f05da41cde3d65a4
Taylor Monacelli <taylormonacelli@gmail.com>
2022-11-03 18:41:41 -0700

    working

---
+++ main.py
@@ -0,0 +1,75 @@
+import argparse
+import datetime
+
+import dateutil.parser
+import pytz
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--xls",
+    action="store_true",
+    default=False,
+    help="output for xls",
+    required=False,
+)
+args = parser.parse_args()
+
+
+def bytesto(bytes, to, bsize=1024):
+    """convert bytes to megabytes, etc.
+    sample code:
+        print('mb= ' + str(bytesto(314575262000000, 'm')))
+
+    sample output:
+        mb= 300002347.946
+    """
+
+    a = {"k": 1, "m": 2, "g": 3, "t": 4, "p": 5, "e": 6}
+    r = float(bytes)
+    for i in range(a[to]):
+        r = r / bsize
+
+    return r
+
+
+tz = pytz.timezone("US/Pacific")
+
+datestamp = "12/2/2022"
+
+end_date = dateutil.parser.parse(datestamp).astimezone(tz)
+
+now = datetime.datetime.now(tz)
+
+diff = end_date - now
+diff = end_date - now + datetime.timedelta(days=1)
+
+
+max = 6 * 10**9
+remaining = 3.12 * 10**9
+remaining = max - 3.17 * 10**9
+bsize = 1_000
+used = max - remaining
+fudge = bsize**2 / 18
+per_day = remaining / diff.days + fudge
+
+mb2 = bytesto(per_day, "m", bsize=bsize)
+
+i = 0
+diff = datetime.timedelta(days=i)
+while now + diff <= end_date:
+    diff = datetime.timedelta(days=i)
+    gb1 = bytesto(used, "g", bsize=bsize)
+    gb2 = bytesto(remaining, "g", bsize=bsize)
+
+    if args.xls:
+        out = f"{(now + diff).date()}\t{mb2}\t{gb1}\t{gb2}"
+    else:
+        out = f"{(now + diff).date()} {mb2:,.2f} {gb1:,.2f}GB {gb2:,.2f}GB"
+    print(out)
+
+    i += 1
+    used += per_day
+    remaining = max - used
+
+
+print(f"from {now.date()} to {end_date.date()} is {diff.days} days")
